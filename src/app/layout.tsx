f14c08ee0fd9d1080b4e7da284fb0f0fc5e55328
GibsonCS <gibson.cruzds@gmail.com>
2025-05-07 21:36:48 -0300

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Gibson Cruz',
+    default:
+      'Gibson Cruz - Software developer, founder, and amateur astronaut',
+  }
+}
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br" className="h-full antialiased">
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
