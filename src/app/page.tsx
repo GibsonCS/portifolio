86ed25d485c7ffb5786c97f8d423dfa4a878db5d
GibsonCS <gibson.cruzds@gmail.com>
2025-05-08 12:26:23 -0300

    refactor: src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,8 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Button } from '@/components/Button'
-import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
-import {
-  GitHubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  XIcon,
-} from '@/components/SocialIcons'
+import { GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
 
 import logoPlanetaria from '@/images/logos/planetaria.svg'
 import image1 from '@/images/photos/image-1.png'
@@ -18,31 +12,30 @@ import image2 from '@/images/photos/image-2.png'
 import image3 from '@/images/photos/image-3.png'
 import image4 from '@/images/photos/image-4.png'
 import image5 from '@/images/photos/image-5.png'
-import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
-import { formatDate } from '@/lib/formatDate'
+import Contact from '@/components/Contact'
 
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
+const images = [
+  {
+    image: image1,
+    alt: 'nodejs',
+  },
+  {
+    image: image2,
+    alt: 'fastify',
+  },
+  {
+    image: image3,
+    alt: 'javascript',
+  },
+  {
+    image: image4,
+    alt: 'typescript',
+  },
+  {
+    image: image5,
+    alt: 'react',
+  },
+]
 
 function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -80,21 +73,6 @@ function ArrowDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-function Article({ article }: { article: ArticleWithSlug }) {
-  return (
-    <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
-        {article.title}
-      </Card.Title>
-      <Card.Eyebrow as="time" dateTime={article.date} decorate>
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-      <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>Read article</Card.Cta>
-    </Card>
-  )
-}
-
 function SocialLink({
   icon: Icon,
   ...props
@@ -105,35 +83,6 @@ function SocialLink({
     <Link className="group -m-1 p-1" {...props}>
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
-  )
-}
-
-function Newsletter() {
-  return (
-    <form
-      action="/thank-you"
-      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(--spacing(2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:ring-4 focus:ring-teal-500/10 focus:outline-hidden sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-    </form>
   )
 }
 
@@ -186,7 +135,7 @@ function Resume() {
   let resume: Array<Role> = [
     {
       company: 'Fesudeperj',
-      title: 'Full-stack developer',
+      title: 'Full-stack Developer',
       logo: logoPlanetaria,
       start: '2021',
       end: {
@@ -196,11 +145,31 @@ function Resume() {
     },
     {
       company: 'Fesudeperj',
-      title: 'IT Assistent',
+      title: 'Tecnólogo em Gestão da TI',
       logo: logoPlanetaria,
       start: '2021',
       end: {
         label: '2022',
+        dateTime: new Date().getFullYear().toString(),
+      },
+    },
+    {
+      company: 'Triunfo',
+      title: 'IT Assist',
+      logo: logoPlanetaria,
+      start: '2018',
+      end: {
+        label: '2020',
+        dateTime: new Date().getFullYear().toString(),
+      },
+    },
+    {
+      company: 'Nova Era',
+      title: 'IT Aux',
+      logo: logoPlanetaria,
+      start: '2015',
+      end: {
+        label: '2018',
         dateTime: new Date().getFullYear().toString(),
       },
     },
@@ -210,14 +179,19 @@ function Resume() {
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
+        <span className="ml-3">Tragetória</span>
       </h2>
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button
+        href="./docs/Gibson_Cruz_Desenvolvedor_Fullstack.pdf"
+        target="_blank"
+        variant="secondary"
+        className="group mt-6 w-full animate-bounce"
+      >
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
@@ -231,17 +205,14 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+        {images.map((image, imageIndex) => (
           <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-              rotations[imageIndex % rotations.length],
-            )}
+            key={imageIndex}
+            className={`relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 ${rotations[imageIndex % rotations.length]}`}
           >
             <Image
-              src={image}
-              alt=""
+              src={image.image}
+              alt={image.alt}
               sizes="(min-width: 640px) 18rem, 11rem"
               className="absolute inset-0 h-full w-full object-cover"
             />
@@ -253,29 +224,30 @@ function Photos() {
 }
 
 export default async function Home() {
-  let articles = (await getAllArticles()).slice(0, 4)
-
   return (
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <div className='flexs flex-col'>
+          <div className="flexs flex-col">
             <span className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Wellcome 👋🏼
+              Wellcome to my world 👋🏼
             </span>
             <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            Me chamo Gibson, sou desenvolvedor de software localizado no Rio de Janeiro.
-            Sou fundador e CEO da CodeLift, onde nas horas vagas, ajudo pessoas e empresas buscarem seu espaço no digital.
+              Me chamo Gibson, sou desenvolvedor de software localizado no Rio
+              de Janeiro. Sou fundador e CEO da CodeLift, onde nas horas vagas,
+              ajudo pessoas e empresas buscarem seu espaço no digital.
             </p>
           </div>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://github.com/gibsoncs" target='_blanck'
+              href="https://github.com/gibsoncs"
+              target="_blanck"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com/in/gibson-cs" target='_blank'
+              href="https://linkedin.com/in/gibson-cs"
+              target="_blank"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
@@ -286,13 +258,14 @@ export default async function Home() {
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+            earum quidem quos, iusto reprehenderit repellat, ipsa explicabo,
+            cumque corrupti maiores quaerat nobis fugit voluptates! Ratione
+            deserunt tempora consequatur iure doloribus!
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Newsletter />
             <Resume />
+            <Contact />
           </div>
         </div>
       </Container>
