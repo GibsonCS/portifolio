dda72e4d1569e83f6848830dbc354fef3ef3657e
GibsonCS <gibson.cruzds@gmail.com>
2026-03-27 21:40:38 -0300

    fix(project): key props

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ const projects = [
   {
     name: 'Sistema Administrativo',
     description:
-    'Projeto fullstack usando Java com Spring no Backend e Thymeleaf no Frontend. (Login: admin | Senha: master123)',
+      'Projeto fullstack usando Java com Spring no Backend e Thymeleaf no Frontend. (Login: admin | Senha: master123)',
     link: {
       href: 'https://administrate-system-c502ed86eb0c.herokuapp.com/login',
       label: 'Sistema Administrativo',
@@ -18,15 +18,14 @@ const projects = [
   {
     name: 'User Auth API',
     description:
-    'Projeto Inteiramente backend de uma API de autenticação e autorização.',
+      'Projeto Inteiramente backend de uma API de autenticação e autorização.',
     link: {
       href: 'https://user-autentication-service-1.onrender.com/api/documentation',
       label: 'User Auth API',
     },
     logo: logoUserLock,
-    sourceCode: "https://github.com/GibsonCS/user-autentication-service"
+    sourceCode: 'https://github.com/GibsonCS/user-autentication-service',
   },
- 
 ]
 
 function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
@@ -51,29 +50,35 @@ export default function Projects() {
         className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
       >
         {projects.map((project) => (
-            <div className='flex flex-col'>
+          <div key={project.name} className="flex flex-col">
             <Card as="li" key={project.name}>
-            <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image
-                src={project.logo}
-                alt=""
-                className="h-8 w-8"
-                unoptimized
+              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                <Image
+                  src={project.logo}
+                  alt=""
+                  className="h-8 w-8"
+                  unoptimized
                 />
-            </div>
-            <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href} target="_blank">
-                {project.name}
-              </Card.Link>
-            </h2>
-            <Card.Description>{project.description}</Card.Description>
-            <p className="relative z-10 mt-2 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-              <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
-            </p>
-          </Card>
-          <Link className='p-2 mt-2' href={project.sourceCode} target='_blank' >Source code</Link>
-          </div>        
+              </div>
+              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                <Card.Link href={project.link.href} target="_blank">
+                  {project.name}
+                </Card.Link>
+              </h2>
+              <Card.Description>{project.description}</Card.Description>
+              <p className="relative z-10 mt-2 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <LinkIcon className="h-6 w-6 flex-none" />
+                <span className="ml-2">{project.link.label}</span>
+              </p>
+            </Card>
+            <Link
+              className="mt-2 p-2"
+              href={project.sourceCode || ''}
+              target="_blank"
+            >
+              Source code
+            </Link>
+          </div>
         ))}
       </ul>
     </SimpleLayout>
